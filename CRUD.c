071903daa3d49c1edcfb3e8923b652e91c4af0d6
Sombra-HY <l.sombra.nascimento@gmail.com>
2022-10-19 12:14:26 -0300

    Menu principal

--- CRUD.c
+++ CRUD.c
@@ -6,4 +6,4 @@
 
 
 
-//funcoes READ
+
--- main.c
+++ main.c
@@ -1,21 +1,51 @@
 #include <stdio.h>
+#include "string.h"
 #include "funcoes.c"
 #include "CRUD.c"
 
-int main(){
-// abre o arquivo para leitura
-    FILE *f = fopen("arquivo.txt","r");
-    char c[400]; // para armazenar o que for lido
-
-    fscanf(f,"%s", &c); // le a primeira linha
-    printf("%s\n", c); // exibe a linha
-    print_vetor_S(c,400);
-    fscanf(f,"%s", &c); // le a próxima linha
-    printf("%s\n", c); // exibe o que foi lido
-    fscanf(f,"%s", &c); // le a primeira linha
-    printf("%s\n", c); // exibe a linha
-    fscanf(f,"%s", &c); // le a próxima linha
-    printf("%s\n", c); // exibe o que foi lido
-
-    fclose(f); // fecha o arquivo
+int Saldo(){
+    printf("saldo");
+    return 1;
 }
+int Extrato(){
+    printf("Estrato");
+    return 1;
+}
+int Transferencia(){
+    printf("Transferencia");
+    return 1;
+}
+
+int Menu(){
+
+    int escolha = 0;
+
+    printf("\nSeja Bem vindo!\n\nEscolha uma das opcoes abaixo: \n\n"
+           "[1] - SALDO   \n"
+           "[2]-  EXTRATO \n"
+           "[3] - TRANFERENCIA  \n"
+           "[9] - ENCERRAR OPERACAO \n");
+    scanf("%d",&escolha);
+
+    return escolha;
+}
+
+int main(){
+
+    typedef int (*function)();
+    function f[]={Menu,Saldo, Extrato, Transferencia};
+    int vetor[]={1,2,3,9};
+
+
+    int escolha = Menu();
+    escolha = (in(escolha,vetor,4))? escolha:0;
+
+    if (escolha==9)
+        printf("Operacao finalizada!");
+    else
+        f[escolha]();
+
+    return 1;
+}
+
+
